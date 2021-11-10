b51a63649b6651f994d2d3241dec27248d43713b
RICKCOYL <rickoburu@gmail.com>
2021-11-10 16:42:39 +0300

    Disable input

--- src/containers/TaskForm.js
+++ src/containers/TaskForm.js
@@ -24,10 +24,10 @@ const TaskForm = () => {
   return (
     <div>
     <form onSubmit={handleSubmit} className="d-flex justify-content-center my-5 input-group">
-      <input disabled={ loading || currentUser } type="text" value={task.title} onChange={e => setTask({ 
+      <input disabled={  currentUser } type="text" value={task.title} onChange={e => setTask({ 
         id: Math.floor(Math.random() * 1111).toString(),
         title: e.target.value })} />
-      <button disabled={ loading || currentUser } type="submit" className="btn btn-primary">Add</button>
+      <button disabled={  currentUser } type="submit" className="btn btn-primary">Add</button>
       </form>
     </div>
   )
